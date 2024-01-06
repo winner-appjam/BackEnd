7ffcb03aa38f3b9b66d8be8dbb6362e527deaa91
도경 <dokyungkim2926@gmail.com>
2024-01-06 15:25:30 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/appjamback/AppjamBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.appjamback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppjamBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppjamBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appjamback/AppjamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appjamback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppjamBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
