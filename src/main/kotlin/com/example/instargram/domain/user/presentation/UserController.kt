fd047968bdb2831dc93343837cb0d32514caab2b
도경 <dokyungkim2926@gmail.com>
2024-01-07 00:02:42 +0900

    asdf

--- src/main/kotlin/com/example/instargram/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/instargram/domain/user/presentation/UserController.kt
@@ -4,8 +4,11 @@ import com.example.instargram.domain.study.service.CreateMemoService
 import com.example.instargram.domain.user.presentation.dto.request.CreateMemoRequest
 import com.example.instargram.domain.user.presentation.dto.request.LoginRequest
 import com.example.instargram.domain.user.presentation.dto.request.SignupRequest
+import com.example.instargram.domain.user.presentation.dto.response.QueryMemoResponse
+import com.example.instargram.domain.user.service.MyMemoQueryService
 import com.example.instargram.domain.user.service.UserLoginService
 import com.example.instargram.domain.user.service.UserSignupService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,7 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody
 class UserController(
     private val userSignupService: UserSignupService,
     private val userLoginService: UserLoginService,
-    private val createMemoService: CreateMemoService
+    private val createMemoService: CreateMemoService,
+    private val myMemoQueryService: MyMemoQueryService
 ) {
 
     @PostMapping
@@ -30,4 +34,7 @@ class UserController(
     @PostMapping("/memo")
     fun createMemo(@RequestBody createMemoRequest: CreateMemoRequest) =
         createMemoService.execute(createMemoRequest)
+
+    @GetMapping("/memo")
+    fun getMemo(): QueryMemoResponse = myMemoQueryService.execute()
 }
